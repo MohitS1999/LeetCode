bbb41abc9b35356938ac3c729155073349ea1920
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2022-12-04 00:41:23 +0530

    Time: 2310 ms (9.10%), Space: 399.5 MB (13.29%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,41 +1,35 @@
+class Pair{
+    char ch;
+    int freq;
+    public Pair(char ch,int freq){
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
 
 class Solution {
-     public String frequencySort(String s) {
-
-//---create a FreqArray to Store frequency of all Characters in String--//
-     
-        int []freq=new int[123];
-        for(int i=0;i<s.length();i++) {freq[(int)s.charAt(i)]++;}
+    public String frequencySort(String s) {
+        int ar[] = new int[123];
         
-//---create an ArrayList of type pair---//        
-        List<pair> q=new ArrayList<>();
-
-//---add elements to the arraylist by creating a new object of pair ---//        
-        for(int i=0;i<123;i++){
-            if(freq[i]>0){
-                q.add(new pair(freq[i],(char)i));
+        for (char ch:s.toCharArray()){
+            ar[(int)ch]++;
+        }
+        
+        List<Pair> list = new ArrayList<>();
+        
+        for (int i=0;i<123;i++){
+            if (ar[i]>0)
+                list.add(new Pair((char)i,ar[i]));
+        }
+        
+        Collections.sort(list,(p1,p2) -> -(p1.freq - p2.freq));
+        
+        String ans = "";
+        for (int i=0;i<list.size();i++){
+            for (int j = 0;j<list.get(i).freq;j++){
+                ans += list.get(i).ch;
             }
         }
-//---sort in descending order--//        
-        
-        Collections.sort(q,(x,y)->{
-            return -(x.val-y.val);
-        });
-// ---use StringBuilder to crete the resultant String ---//       
-       
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<q.size();i++){
-            int n=q.get(i).val;
-            for(int j=0;j<n;j++)sb.append(q.get(i).c);
-        }
-        return sb.toString();
-        }
-//--- a class of type pair that holds each character and its frequency in String input--//    
-  class pair{
-      char c; int val;
-      pair(int val,char c){
-          this.c=c;
-          this.val=val;
-      }
-  }  
+        return ans;
+    }
 }
