978f7612c7a9009b3da95e788d1581174d32521f
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2022-12-06 00:32:41 +0530

    Time: 52 ms (39.13%), Space: 123.5 MB (8.76%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> list;
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        TreeNode lca = findLCA(root,startValue,destValue);
+        list = new ArrayList<>();
+        StringBuilder sbStart = new StringBuilder();
+        StringBuilder sbDest = new StringBuilder();
+        // finding the startValue
+        find(lca,sbStart,startValue);
+        find(lca,sbDest,destValue);
+        return "U".repeat(list.get(0).length()) + list.get(1);
+        
+        
+        
+    }    
+    
+    private TreeNode findLCA(TreeNode root,int p,int q){
+        if (root == null || root.val == p || root.val == q) return root;
+        
+        TreeNode left = findLCA(root.left,p,q);
+        TreeNode right = findLCA(root.right,p,q);
+        
+        if (left == null) return right;
+        else if (right == null) return left;
+        return root;
+    }
+    
+    private void find(TreeNode root,StringBuilder sb,int data){
+       
+        if (root.val == data){
+            list.add(sb.toString());
+            return;
+        }
+        if(root.left!=null)
+            find(root.left,sb.append("L"),data);
+        if(root.right!=null)
+            find(root.right,sb.append("R"),data);
+        
+        sb.deleteCharAt(sb.length()-1);
+        return;
+    }
+   
+}
