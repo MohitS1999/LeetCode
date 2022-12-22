3e43ffd49e6269d25863eb0a303d042cfb385f5a
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2022-12-23 00:03:50 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int d= Integer.parseInt(starr[1]);
+    		
+            int[] arr=new int[n];
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		  arr[j]= Integer.parseInt(starr1[j]);
+    		}
+    		
+    		//calling rotateArr() function
+            new Solution().rotateArr(arr, d, n);
+    		StringBuffer sb=new StringBuffer();
+    		
+    		//printing the elements of the array
+            for(int t1=0;t1<n;t1++)
+                sb.append(arr[t1]+" ");
+            out.println(sb);
+         }
+         out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int ar[], int d, int n)
+    {
+        d = d%n;
+        reverse(0,n-1,ar);
+        reverse(0,n-d-1,ar);
+        reverse(n-d,n-1,ar);
+    }
+    private static void reverse(int i,int j,int ar[]){
+        while (i < j){
+            int temp = ar[i];
+            ar[i] = ar[j];
+            ar[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
