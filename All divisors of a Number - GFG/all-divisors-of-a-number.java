6d3fe26203da849c7a0a37dd919b8b45bef9415e
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2022-12-23 20:27:28 +0530

    Added solution - LeetHub

---
+++ All divisors of a Number - GFG/all-divisors-of-a-number.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            obj.print_divisors(n);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static void print_divisors(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=1;i<=n/2;i++){
+            if (n % i == 0) sb.append(i+" ");
+        }
+        sb.append(n);
+        System.out.print(sb);
+        
+    }
+}
